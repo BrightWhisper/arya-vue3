65819644ca25d13318b47771a8dc0392577e6925
wangshuyu01 <wangshuyu01@58.com>
2023-02-06 16:07:40 +0800

    测试as const

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,25 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const PropsType = {
+  mag: String,
+  age: {
+    type: Number,
+    required: true,
+  },
+} as const
+export default defineComponent({
+  name: 'HelloWorld',
+  props: PropsType,
+  mounted() {
+    this.age
+  },
+})
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <div class="hello">
+    {{ age }}
   </div>
 </template>
 
